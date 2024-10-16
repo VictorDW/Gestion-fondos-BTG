962d253c0605d84ca440a6c18599a5495bb9382c
victorADW <victor.agudelo@pragma.com.co>
2024-10-16 17:36:18 -0500

    refactor: se agrego el mapper para realizar el mapeo entre persistencia y dominio

---
+++ src/main/java/com/victordw/btg/adapter/driven/mongodb/mapper/IFundMapper.java
@@ -0,0 +1,10 @@
+package com.victordw.btg.adapter.driven.mongodb.mapper;
+
+import com.victordw.btg.domain.model.InvestmentFund;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface IFundMapper {
+
+	InvestmentFund toModel(InvestmentFund investmentFund);
+}
