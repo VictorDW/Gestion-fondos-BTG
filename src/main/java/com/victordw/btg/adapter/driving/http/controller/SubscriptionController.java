ffd03e47138c820d4dc2884c8ae8bbd8d0c07a2f
victorADW <victor.agudelo@pragma.com.co>
2024-10-18 09:56:33 -0500

    feature: se agrego el endpoint para cancelar una suscripción

--- src/main/java/com/victordw/btg/adapter/driving/http/controller/SubscriptionController.java
+++ src/main/java/com/victordw/btg/adapter/driving/http/controller/SubscriptionController.java
@@ -1,5 +1,6 @@
 package com.victordw.btg.adapter.driving.http.controller;
 
+import com.victordw.btg.adapter.driving.http.dto.request.FundCancellationRequest;
 import com.victordw.btg.adapter.driving.http.dto.request.FundSubscribedRequest;
 import com.victordw.btg.adapter.driving.http.mapper.request.IFundSubscribeMapperRequest;
 import com.victordw.btg.domain.api.ISubscriptionServicePort;
@@ -15,14 +16,24 @@ public class SubscriptionController {
 
 	private final IFundSubscribeMapperRequest fundSubscribeMapperRequest;
 	private final ISubscriptionServicePort clientServicePort;
+	private final ISubscriptionServicePort clientServicesPort;
 
 	@PostMapping("/{id}")
 	ResponseEntity<Void> subscribeFund(
 			@PathVariable("id") String clientId,
-			@RequestBody FundSubscribedRequest fundSubscribedRequest
+			@RequestBody FundSubscribedRequest request
 	) {
-		FundSubscribed fundSubscribed = fundSubscribeMapperRequest.toModel(fundSubscribedRequest);
+		FundSubscribed fundSubscribed = fundSubscribeMapperRequest.toModel(request);
 		clientServicePort.addSubscription(clientId, fundSubscribed);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("/{id}")
+	ResponseEntity<Void> cancellationFund(
+			@PathVariable("id") String clientId,
+			@RequestBody FundCancellationRequest request
+	) {
+		clientServicesPort.cancellationSubscription(clientId, request.fundId());
 		return ResponseEntity.ok().build();
 	}
 }
---
+++ src/main/java/com/victordw/btg/adapter/driving/http/dto/request/FundCancellationRequest.java
@@ -0,0 +1,6 @@
+package com.victordw.btg.adapter.driving.http.dto.request;
+
+public record FundCancellationRequest(
+		Long fundId
+) {
+}
