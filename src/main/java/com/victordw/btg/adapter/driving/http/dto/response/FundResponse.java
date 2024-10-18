271aeea5b5859a52ca06d370ac081fb199d43a27
victorADW <victor.agudelo@pragma.com.co>
2024-10-18 18:55:52 -0500

    feature: se agrego el id en la respuesta al listar los fondos existentes

--- src/main/java/com/victordw/btg/adapter/driving/http/dto/response/FundResponse.java
+++ src/main/java/com/victordw/btg/adapter/driving/http/dto/response/FundResponse.java
@@ -3,6 +3,7 @@ package com.victordw.btg.adapter.driving.http.dto.response;
 import java.math.BigDecimal;
 
 public record FundResponse(
+		Long id,
 		String name,
 		BigDecimal minimumAmount,
 		String category
