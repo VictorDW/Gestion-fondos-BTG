84b65f0b0824e40a6def2ceb5fc4c83de5959152
victorADW <victor.agudelo@pragma.com.co>
2024-10-17 20:19:44 -0500

    feature: se creo la logica para inyectar la implementación según la preferencia del cliente

--- src/main/java/com/victordw/btg/configuration/ApplicationConfig.java
+++ src/main/java/com/victordw/btg/configuration/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.victordw.btg.configuration;
 
+import com.victordw.btg.adapter.driven.mail.adapter.EmailAdapter;
+import com.victordw.btg.adapter.driven.sms.adapter.SmsAdapter;
 import com.victordw.btg.domain.api.ISubscriptionServicePort;
 import com.victordw.btg.domain.api.IFundServiceBasic;
 import com.victordw.btg.domain.api.IFundServicesPort;
@@ -9,10 +11,14 @@ import com.victordw.btg.domain.api.usecase.FundUseCase;
 import com.victordw.btg.domain.api.usecase.TransactionUseCase;
 import com.victordw.btg.domain.spi.IClientPersistencePort;
 import com.victordw.btg.domain.spi.IFundPersistencePort;
+import com.victordw.btg.domain.spi.ISendNotificationPort;
 import com.victordw.btg.domain.spi.ITransactionPersistencePort;
+import com.victordw.btg.domain.util.ConstantDomain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import java.util.function.Function;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,6 +27,7 @@ public class ApplicationConfig {
 	private final IFundPersistencePort fundPersistencePort;
 	private final IClientPersistencePort clientPersistencePort;
 	private final ITransactionPersistencePort transactionPersistencePort;
+	private final JavaMailSender javaMailSender;
 
 	@Bean
 	public IFundServicesPort fundServicesPort() {
@@ -31,10 +38,20 @@ public class ApplicationConfig {
 	public ITransactionService transactionService() {
 		return new TransactionUseCase(transactionPersistencePort);
 	}
+
 	@Bean
-	public ISubscriptionServicePort clientServicesPort() {
-		return new ClientUseCase(clientPersistencePort, (IFundServiceBasic) fundServicesPort(), transactionService());
+	public Function<String, ISendNotificationPort> defineShippingMethod() {
+		return notificationPreference -> {
+			if (!ConstantDomain.SMS.equalsIgnoreCase(notificationPreference)) {
+				return new EmailAdapter(javaMailSender);
+			}
+			return new SmsAdapter();
+		};
 	}
 
+	@Bean
+	public ISubscriptionServicePort clientServicesPort() {
+		return new ClientUseCase(clientPersistencePort, (IFundServiceBasic) fundServicesPort(), transactionService(), defineShippingMethod());
+	}
 
 }
